225b3f5a21284fbc16bf7a9e6b74e7fbc430c948
cheech <bacmive@sohu.com>
2021-07-10 11:57:00 +0800

    all

---
+++ vueblog/src/main/java/com/tsj/mapper/BlogMapper.java
@@ -0,0 +1,16 @@
+package com.tsj.mapper;
+
+import com.tsj.entity.Blog;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author crazy seikwan
+ * @since 2021-07-08
+ */
+public interface BlogMapper extends BaseMapper<Blog> {
+
+}
